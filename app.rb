ea61c3845a49ee8c06cd7bd983c698460870f067
N-leaner <yahoo@pizd.ec>
2016-06-13 16:54:34 +0300

    Execute simple query

--- app.rb
+++ app.rb
@@ -1,1 +1,11 @@
 require 'sqlite3'
+db = SQLite3::Database.new 'test.db'
+
+#db.execute "INSERT INTO Cars (Name, Prise) Values ('Jaguar',70000)"
+
+arr = db.execute "SELECT * FROM Cars"
+arr.each do |str|
+	puts str.join(', ')
+end	
+
+db.close
